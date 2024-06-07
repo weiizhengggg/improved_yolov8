be8f00425030d48d63e09bebb809b67134662cde
weiizhengggg <kikuchitouma00@gmail.com>
2024-06-07 12:48:43 +0800

    Add CBAM

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -49,6 +49,7 @@ from ultralytics.nn.modules import (
     Segment,
     Silence,
     WorldDetect,
+    CBAM
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -925,6 +926,11 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             args = [c1, c2, *args[1:]]
         elif m is CBFuse:
             c2 = ch[f[-1]]
+        elif m is CBAM: # add elif for CBAM
+            c1, c2 = ch[f], args[0]
+            if c2 != nc:  # if c2 is not equal to number of classes (i.e. for Classify() output)
+                c2 = make_divisible(c2 * width, 8)
+            args = [c1, *args[1:]]
         else:
             c2 = ch[f]
 
